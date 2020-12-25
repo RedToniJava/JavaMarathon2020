f8ad7051fa88175ff537a1a014615f3496c7d759
RedToniJava <krasnovkaa@yandex.ru>
2020-12-25 13:54:18 +0300

    Complete day2 tasks

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,8 +2,8 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-       int java=0;
-        while( java<10){
+        int java = 0;
+        while (java < 10) {
             java++;
             System.out.print("JAVA ");
         }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,29 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a >= b) {
+            System.out.print("Некорректный ввод");
+        }
+
+        while (a < b) {
+            a++;
+            if (a == b) break;
+            if (a % 5 == 0 & a % 10 != 0)
+                System.out.print(a + " ");
+
+        }
+
 
     }
+
+
 }
+
