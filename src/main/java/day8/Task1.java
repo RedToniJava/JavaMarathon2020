c0cd3ca7afb116716f3fd36d7a1fa7c8ca169ef0
RedToniJava <krasnovkaa@yandex.ru>
2021-01-02 19:51:03 +0300

    Complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,17 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String string = " ";
+        for (int i = 0; i <= 20000; i++) {
+            string = string + i + " ";
 
+        }
+        System.out.println(string);
+
+        StringBuilder sb = new StringBuilder(" ");
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb.toString());
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,65 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane = new Airplane("Boeing", 2000, 150, 10000);
+        System.out.println(airplane);
 
     }
 }
+
+class Airplane {
+    private String producer;
+    private int year;
+    private double length;
+    private double weight;
+    private int fuel;
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getFuel() {
+        return fuel;
+
+    }
+
+    public Airplane(String producer, int year, double length, double weight) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+
+    }
+
+    void fullUp(int n) {
+        fuel += n;
+
+    }
+
+    public String toString() {
+
+        return "Изготовитель: " + producer + "; год выпуска: " + year + "; длинна: " + length + "; вес: " + weight;
+    }
+
+
+    void info() {
+        System.out.println("Изготовитель: " + producer + "; год выпуска: " + year + "; длинна: " + length + "; вес: " + weight);
+    }
+}
+
